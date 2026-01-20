8d654324dd94258bbd884720d0da362e65db4479
lennardDenby <lennardDenby>
2026-01-20 13:29:52 +0100

    La til ressurser for uke 0

---
+++ uke0 - Python til Java/ForWhile.java
@@ -0,0 +1,13 @@
+public class ForWhile {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i ++) {
+            System.out.println("Hei" + i);
+        }
+
+        int num = 0;
+        while (num < 10) {
+            System.out.println("Hei" + num);
+            num++;
+        }
+    }
+}
